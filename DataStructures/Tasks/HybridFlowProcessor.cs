9c0ddc5bca20fa83b9c531c6a91fb9e9701bb43f
Asadbek Khasanov <asadbek1khasanov@gmail.com>
2022-01-23 23:38:06 +0500

    added implementation to HybridFlowProcessor

--- DataStructures/Tasks/HybridFlowProcessor.cs
+++ DataStructures/Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,43 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private DoublyLinkedList<T> _data = new DoublyLinkedList<T>();
+
+        public int Length => _data.Length;
+        public bool IsEmpty => _data.Length == 0;
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T element = _data.ElementAt(0);
+            _data.RemoveAt(0);
+            return element;
         }
 
-        public void Enqueue(T item)
+        public void Enqueue(T toBeAdded)
         {
-            throw new NotImplementedException();
+            _data.Add(toBeAdded);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T element = _data.ElementAt(_data.Length - 1);
+            _data.RemoveAt(_data.Length - 1);
+            return element;
         }
 
-        public void Push(T item)
+        public void Push(T toBeAdded)
         {
-            throw new NotImplementedException();
+            _data.Add(toBeAdded);
         }
     }
 }
