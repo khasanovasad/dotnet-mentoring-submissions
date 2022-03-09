49356cf613805e1e12929d3862c53c43c3977063
Asadbek Khasanov <asad1khasanov@gmail.com>
2022-03-09 13:30:13 +0500

    linq module

---
+++ LINQ/README.md
@@ -0,0 +1,39 @@
+# Coding Task
+## Description
+
+You are given a solution (LINQ.zip) containing the task project and the tests. Your task is to implement methods in LinqTask class following instructions from the list below until the tests are green. 
+
+Queries: 
+1. Select the customers whose total turnover (the sum of all orders) exceeds a certain value. 
+
+2. For each customer make a list of suppliers located in the same country and the same city. Compose queries with and without grouping. 
+
+3. Find all customers with the sum of all orders that exceed a certain value. 
+
+4. Select the clients, including the date of their first order. 
+
+5. Repeat the previous query but order the result by year, month, turnover (descending) and customer name. 
+
+6. Select the clients which either have:
+ <br/>
+ &nbsp;a. non-digit postal code
+ <br/>
+ &nbsp;b. undefined region
+ <br/>
+ &nbsp;c. operator code in the phone is not specified (does not contain parentheses) 
+ <br/>
+
+7. Group the products by category, then by availability in stock with ordering by cost. 
+
+8. Group the products by “cheap”, “average” and “expensive” following the rules:
+ <br/>
+ &nbsp;a. From 0 to cheap inclusive
+ <br/>
+ &nbsp;b. From cheap exclusive to average inclusive
+ <br/>
+ &nbsp;c. From average exclusive to expensive inclusive 
+ <br/>
+
+9. Calculate the average profitability of each city (average amount of orders per customer) and average rate (average number of orders per customer from each city). 
+
+10. Build a string of unique supplier country names, sorted first by length and then by country.
---
+++ LINQ/Task1/LinqTask.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Task1.DoNotChange;
+
+namespace Task1
+{
+    public static class LinqTask
+    {
+        public static IEnumerable<Customer> Linq1(IEnumerable<Customer> customers, decimal limit)
+        {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            var result = from customer in customers
+                         where (from order in customer.Orders select order.Total).Sum() > limit
+                         select customer;
+            return result;
+        }
+
+        public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2(
+            IEnumerable<Customer> customers,
+            IEnumerable<Supplier> suppliers
+        )
+        {
+            if (customers is null || suppliers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            var result = from customer in customers
+                         select (customer, (from supplier in suppliers
+                                            where supplier.Country == customer.Country && supplier.City == customer.City
+                                            select supplier));
+            return result;
+        }
+
+        public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2UsingGroup(
+            IEnumerable<Customer> customers,
+            IEnumerable<Supplier> suppliers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
+        {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            var result = from customer in customers
+                         where (from order in customer.Orders select order.Total).Sum() > limit
+                         select customer;
+            return result;
+        }
+
+        public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq4(IEnumerable<Customer> customers)
+        {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            /*            var result = from customer in customers
+                                     select (customer, customer.Orders.Min(order => order.OrderDate));
+                        return result;
+            */
+
+            var result = customers.SkipWhile(customer => customer.Orders.Count() == 0).Select(customer =>
+            {
+                return (customer, customer.Orders.Min(Order => Order.OrderDate));
+            });
+            return result;
+        }
+
+        public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
+            IEnumerable<Customer> customers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
+        {
+            /* example of Linq7result
+
+             category - Beverages
+	            UnitsInStock - 39
+		            price - 18.0000
+		            price - 19.0000
+	            UnitsInStock - 17
+		            price - 18.0000
+		            price - 19.0000
+             */
+
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(decimal category, IEnumerable<Product> products)> Linq8(
+            IEnumerable<Product> products,
+            decimal cheap,
+            decimal middle,
+            decimal expensive
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(string city, int averageIncome, int averageIntensity)> Linq9(
+            IEnumerable<Customer> customers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Linq10(IEnumerable<Supplier> suppliers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
